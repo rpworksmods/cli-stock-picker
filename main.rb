fdb1cd2460c27aecac56bb8245cf098e1bc058db
rpworksmods <kyej1023@gmail.com>
2026-01-29 17:07:52 +0000

    User-friendly Input for Stock Picker
    - Welcome message in console
    - Prompt user for number of days
    - Loop to get stock prices for each day
    - Store prices in an array
    - Call stock_picker function with user-provided prices
    - Format output with dividers for clarity

--- main.rb
+++ main.rb
@@ -16,15 +16,25 @@ def stock_picker(days)
     end
   end
 
-  puts best_days
+  puts "Buy on day #{best_days[0]} at price #{days[best_days[0]]}, sell on day #{best_days[1]} at price #{days[best_days[1]]}, for a profit of #{best_profit}."
 
 end
 
-stock_picker([17,19,3,6,9,15,8,6,1,10])
+puts "### Stock Picker ###"
+puts "Given an array of stock prices where the index represents the day,"
+puts "this program determines the best day to buy and the best day to sell"
+puts "to maximize profit. It outputs the indices of the buy and sell days."
+puts "For example, given the array: [17,19,3,6,9,15,8,6,1,10]"
+puts "The output will be: [2, 5] (buy on day 2 at price 3, sell on day 5 at price 15)"
+puts "---------------------"
 
-# Post-MVP:
-# Beautify user input
-# Request using gets in a user-friendly format:
-# - Number of days: (To establish how many items in the array)
-# - Loop through array and use gets to obtain price for that day
-# - Calculate, and output results in a formatted way using dividers
+puts "Please input the amount of days you'd like to analyze (e.g. 10):"
+num_days = gets.chomp.to_i
+prices = []
+num_days.times do |i|
+  puts "Enter the price for day #{i}:"
+  price = gets.chomp.to_i
+  prices << price
+end
+puts "Analyzing prices: #{prices}"
+stock_picker(prices)
