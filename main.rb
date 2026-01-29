e9d10881b3873ac310fca51281ee08910ef8ad69
rpworksmods <kyej1023@gmail.com>
2026-01-29 15:49:48 +0000

    Environment Setup & Planned Flow via Comments

---
+++ main.rb
@@ -0,0 +1,17 @@
+def stock_picker(days)
+  # Use reduce() to return two days in an array representing best day to buy and best day to sell
+  # Best day to sell: lowest number in array
+  # Best day to buy: highest number in array
+  ### BUT!! - Must buy before selling!!! #####
+  # Use array item's index to determine which day each price relates to (0,1,2,3,4 e.t.c.)
+  
+  
+
+end
+
+# Post-MVP:
+# Beautify user input
+# Request using gets in a user-friendly format:
+# - Number of days: (To establish how many items in the array)
+# - Loop through array and use gets to obtain price for that day
+# - Calculate, and output results in a formatted way using dividers
